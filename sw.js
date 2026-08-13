d5f165d4d0c259bedafb5d982e104e5a1566a59d
jpajdy <pajducak96@gmail.com>
2026-08-13 16:19:19 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Mapa sebe — service worker */
-var CACHE = "zjh-pwa-v23";
+var CACHE = "zjh-pwa-v27";
 var SHELL = ["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png","./mapa.html","./quests.json"];
 
 self.addEventListener("install", function(e){
